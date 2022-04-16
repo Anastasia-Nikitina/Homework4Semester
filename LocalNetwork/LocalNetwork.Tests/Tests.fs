15e7a54c94a44bcb9c0e837f3023cf7e267f63b4
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-04-16 21:37:21 +0300

    local network init

---
+++ LocalNetwork/LocalNetwork.Tests/Tests.fs
@@ -0,0 +1,44 @@
+module LocalNetwork.Tests
+
+open NUnit.Framework
+open LocalNetwork
+open FsUnit
+open Foq
+
+[<Test>]
+let testForOneInfected () =
+    let comp1 = Computer(1, Windows, true)
+    let comp2 = Computer(2, Linux, false)
+    let comp3 = Computer(3, MacOS, false)
+    let network1 = Network([comp1; comp2; comp3], [(1, 2); (2,3)])
+    network1.Start
+    comp1.IsInfected |> should equal true
+    comp2.IsInfected |> should equal true
+    comp3.IsInfected |> should equal true
+
+[<Test>]
+let testForZeroInfected () =
+    let comp1 = Computer(1, Windows, false)
+    let comp2 = Computer(2, Linux, false)
+    let comp3 = Computer(3, MacOS, false)
+    let network2 = Network([comp1; comp2; comp3], [(1, 3);(1, 2)])
+    network2.Start
+    comp1.IsInfected |> should equal false
+    comp2.IsInfected |> should equal false
+    comp3.IsInfected |> should equal false
+
+[<Test>]
+let testWithMock () =
+    let comp1 = Computer(1, Windows, false)
+    let comp2 = Computer(2, Linux, true)
+    let comp3 = Computer(3, MacOS, false)
+    let randomizer = Mock<System.Random>()
+                             .Setup(fun x -> <@ x.NextDouble() @>)
+                             .Returns(1)
+                             .Create()
+    let network3 = Network([comp1; comp2; comp3], [(1, 2); (2, 3)], randomizer)
+    network3.Start
+    comp1.IsInfected |> should equal true
+    comp2.IsInfected |> should equal true
+    comp3.IsInfected |> should equal true
+                          
---
+++ LocalNetwork/LocalNetwork/Functions.fs
@@ -0,0 +1,69 @@
+﻿module LocalNetwork
+
+open System
+
+type OS  =
+    | Windows
+    | Linux
+    | MacOS
+
+type Computer(name: int, os: OS, isInfected: bool) =
+    let probability () =
+        match os with
+        | Windows -> 0.7
+        | Linux -> 0.5
+        | MacOS -> 0.6
+    member this.Name = name
+    member this.OS = os
+    member val IsInfected = isInfected with get, set
+    member val NewInfected = false with get, set
+    member this.Probability = probability
+    
+type Network (computers: List<Computer>, communication: List<int*int>, ?randomizer) =
+    let random =
+        match randomizer with
+        | Some x -> x
+        | None -> Random()
+    member this.Computers = computers
+    member this.Infect(comp1: Computer, comp2: Computer) =
+        let comp1Inf = comp1.IsInfected
+        let comp2Inf = comp2.IsInfected
+        if (communication |> List.contains((comp1.Name, comp2.Name))) &&
+            (comp1Inf && not comp2Inf) || (not comp1Inf && comp2Inf)
+        then
+            if (comp1.IsInfected) && (comp2.Probability() < random.NextDouble())
+            then
+                comp2.IsInfected <- true
+                comp2.NewInfected <- true
+            if (comp2.IsInfected) && (comp2.Probability() < random.NextDouble())
+            then
+                comp1.IsInfected <- true
+                comp1.NewInfected <- true
+        
+    member this.Communication = communication
+    member val IsFinalState = false with get, set 
+    member this.Step =
+        this.Computers |> List.iter(fun x -> x.NewInfected <- false )
+        let step () =
+            let rec inner (list: List<Computer>) =
+                match list with
+                | [] ->
+                    computers
+                | head :: tail ->
+                    tail |> List.iter(fun x -> this.Infect(head, x))
+                    inner (tail |> List.filter(fun x -> not x.NewInfected))      
+            inner computers |> ignore
+        step()   
+    
+    member this.Start =
+        let mutable numberOfStep = 1
+        if (this.Computers |> List.filter(fun x -> x.IsInfected)).Length <> 0
+        then
+            while this.Computers.Length <> (this.Computers |> List.filter(fun x -> x.IsInfected)).Length do
+                this.Step
+                printfn $"Step number %A{numberOfStep}"
+                this.Computers
+                |> List.iter(fun x -> printfn $"Computer %A{x.Name}. Is infected: %A{x.IsInfected}")
+                numberOfStep <- numberOfStep + 1
+        printfn "Network in final state"      
+    
